74b8deabb3ffc93020d3e1dfd4358547627f33d1
ayushrskiaa <ayushkumar2263@iiitmanipur.ac.in>
2025-03-28 12:40:34 +0530

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,7 @@ export default function Header() {
                 <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <Link to="/" className="flex items-center ">
+                        
                             <img
                                 src="src\assets\logo.png"
                                 className="mr-5 h-12"
