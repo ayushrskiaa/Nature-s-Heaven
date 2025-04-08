9f5d61a05594961fa1da23d1c7fd3bb97b2e7f54
ayushrskiaa <ayushkumar2263@iiitmanipur.ac.in>
2025-04-08 13:56:43 +0530

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,31 @@
 /* eslint-disable react/no-unescaped-entities */
 // import React from "react";
 
+import React from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
 export default function About() {
+
+  const [open, setOpen] = React.useState(1);
+
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);
+
   return (
     <div className="py-16 bg-gradient-to-bl from-emerald-800 to-emerald-500">
       <div className="container m-auto px-6  md:px-12 xl:px-6">
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12 ">
           <div className=" w-200 h-80 overflow-hidden rounded-lg shadow-xl dark:shadow-gray-800">
-            <img             
+            <img
               src="https://images.pexels.com/photos/27896591/pexels-photo-27896591/free-photo-of-green-leaves-wallpaper.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
               alt="image"
             />
-            
           </div>
 
-          
           <div className="md:7/12 lg:w-6/12 py-22">
-
-       {/* <div className="w-200 h-40 overflow-hidden mb-20">
+            {/* <div className="w-200 h-40 overflow-hidden mb-20">
        <img  src="https://images.pexels.com/photos/27896591/pexels-photo-27896591/free-photo-of-green-leaves-wallpaper.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
 
        </div> */}
@@ -36,13 +44,53 @@ export default function About() {
               Together, let's cherish and protect the world we share. J
             </p>
             <p className="mt-4 text-white">
-            Join us on this journey of awe and appreciation — because nature’s beauty is a treasure for all.
-
-
+              Join us on this journey of awe and appreciation — because nature’s
+              beauty is a treasure for all.
             </p>
           </div>
         </div>
       </div>
+
+
+
+      <div className="container m-auto px-6  md:px-12 xl:px-6 py-16 text-white">
+        <h1 className="text-white text-3xl  font-bold text-center ">FAQs</h1>
+
+
+        <div className="py-8">
+          <Accordion open={open === 1}>
+            <AccordionHeader onClick={() => handleOpen(1)}> How do I book a hotel?</AccordionHeader>
+            <AccordionBody className="text-white text-md">
+              Booking a hotel is simple! Browse through our list of available
+              hotels, select your preferred dates, and click "Book Now." Follow
+              the prompts to complete your reservation.
+            </AccordionBody>
+          </Accordion>
+          <Accordion open={open === 2}>
+            <AccordionHeader onClick={() => handleOpen(2)}>
+              Can I cancel or modify my booking?
+            </AccordionHeader>
+            <AccordionBody className="text-white text-md">
+              Yes, you can cancel or modify your booking depending on the hotel's
+              cancellation policy. Visit the "My Bookings" section, select your
+              reservation, and follow the instructions to make changes.
+            </AccordionBody>
+          </Accordion>
+          <Accordion open={open === 3}>
+            <AccordionHeader onClick={() => handleOpen(3)}>
+              Are there any hidden fees?
+            </AccordionHeader>
+            <AccordionBody className="text-white text-md">
+              No, we believe in transparency. All fees, including taxes and
+              service charges, are displayed upfront during the booking process.
+            </AccordionBody>
+          </Accordion>
+        </div>
+
+      </div>
+
+
+
     </div>
   );
 }
